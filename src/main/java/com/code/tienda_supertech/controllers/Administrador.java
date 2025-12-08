21813b94e58fe6d62a95264a3c2638c18011ffd3
DbodoR <dieegoo661@gmail.com>
2025-12-07 21:49:02 -0500

    creacion de la primera vista,home de la aplicacion, integracion de bootstrap y template de header y footer

---
+++ src/main/java/com/code/tienda_supertech/controllers/Administrador.java
@@ -0,0 +1,16 @@
+package com.code.tienda_supertech.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class Administrador {
+
+    @GetMapping("")
+    public String home(){
+
+        return "admin/home";
+    }
+}
