bb73c510b179accf2b8ccec23017a053c58ef5f7
DbodoR <dieegoo661@gmail.com>
2025-12-08 19:42:31 -0500

    creacion template para vistas de usuario, creacion de vista productohome para visualizar un producto

--- src/main/java/com/code/tienda_supertech/controllers/HomeController.java
+++ src/main/java/com/code/tienda_supertech/controllers/HomeController.java
@@ -1,15 +1,20 @@
 package com.code.tienda_supertech.controllers;
 
 import com.code.tienda_supertech.services.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -18,5 +23,11 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        logger.info("Id producto enviado como parámetro {}",id);
+        return "usuario/productohome";
     }
 }
