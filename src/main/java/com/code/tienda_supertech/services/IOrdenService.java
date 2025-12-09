3e8b1177db5da8720abb38a17d79ac13242fb694
DbodoR <dieegoo661@gmail.com>
2025-12-09 00:21:19 -0500

    generacion numero de orden

--- src/main/java/com/code/tienda_supertech/services/IOrdenService.java
+++ src/main/java/com/code/tienda_supertech/services/IOrdenService.java
@@ -2,6 +2,9 @@ package com.code.tienda_supertech.services;
 
 import com.code.tienda_supertech.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
     Orden save(Orden orden);
+    List<Orden> findAll();
 }
