9b98d6d4adc5ed81f15f90dbc19274672f62392d
DbodoR <dieegoo661@gmail.com>
2025-12-09 02:12:48 -0500

    creacion vista login de usuario, validacion temporal para pruebas

--- src/main/java/com/code/tienda_supertech/controllers/UsuarioController.java
+++ src/main/java/com/code/tienda_supertech/controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package com.code.tienda_supertech.controllers;
 
 import com.code.tienda_supertech.model.Usuario;
 import com.code.tienda_supertech.services.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -30,6 +33,28 @@ public class UsuarioController {
         logger.info("Usuario registro: {}", usuario);
         usuario.setType("USER");
         usuarioService.save(usuario);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session){
+        logger.info("Usuario accede: {}", usuario);
+
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+
+        if (user.isPresent()){
+            session.setAttribute("idusuario", user.get().getId());
+            if (user.get().getType().equals("ADMIN")){
+                return "redirect:/admin";
+            }
+        }else {
+            logger.info("Usuario no encontrado");
+        }
 
         return "redirect:/";
     }
