941939420fb7821d54fd524b074c3ce199f93705
DbodoR <dieegoo661@gmail.com>
2025-12-09 18:33:45 -0500

    implementacion de vista detallecompra y ver detalle de la compra del usuario

--- src/main/java/com/code/tienda_supertech/services/IOrdenService.java
+++ src/main/java/com/code/tienda_supertech/services/IOrdenService.java
@@ -4,10 +4,12 @@ import com.code.tienda_supertech.model.Orden;
 import com.code.tienda_supertech.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrdenService {
     Orden save(Orden orden);
     List<Orden> findAll();
     String generarNumeroOrden();
     List<Orden> findByUsuario(Usuario usuario);
+    Optional<Orden> findById(Integer id);
 }
