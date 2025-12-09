1e40d6aa6d1efff1a739d28490e42d452639c890
DbodoR <dieegoo661@gmail.com>
2025-12-08 20:01:00 -0500

    visualizacion del producto seleccionado en la vista productohome

--- src/main/java/com/code/tienda_supertech/controllers/HomeController.java
+++ src/main/java/com/code/tienda_supertech/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.code.tienda_supertech.controllers;
 
+import com.code.tienda_supertech.model.Producto;
 import com.code.tienda_supertech.services.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -26,8 +29,14 @@ public class HomeController {
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id, Model model) {
         logger.info("Id producto enviado como parámetro {}",id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
+
         return "usuario/productohome";
     }
 }
