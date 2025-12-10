432671089629a19ed1ce8bebd79d2f15ace58bd7
DbodoR <dieegoo661@gmail.com>
2025-12-10 01:07:50 -0500

    vista detalle de la orden con funcionalidad para administrador

--- src/main/java/com/code/tienda_supertech/controllers/AdministradorController.java
+++ src/main/java/com/code/tienda_supertech/controllers/AdministradorController.java
@@ -1,13 +1,17 @@
 package com.code.tienda_supertech.controllers;
 
+import com.code.tienda_supertech.model.Orden;
 import com.code.tienda_supertech.services.IOrdenService;
 import com.code.tienda_supertech.services.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import com.code.tienda_supertech.model.Producto;
 import com.code.tienda_supertech.services.IProductoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,6 +19,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/admin")
 public class AdministradorController {
+
+    private Logger logger = LoggerFactory.getLogger(AdministradorController.class);
 
     @Autowired
     private IProductoService productoService;
@@ -46,5 +52,14 @@ public class AdministradorController {
         model.addAttribute("ordenes", ordenService.findAll());
 
         return "admin/ordenes";
+    }
+
+    @GetMapping("/detalleorden/{id}")
+    public String detalleOrden(Model model, @PathVariable Integer id){
+        logger.info("detalle orden: {}", id);
+        Orden orden = ordenService.findById(id).get();
+        model.addAttribute("detalle", orden.getDetalle());
+
+        return "admin/detalleorden";
     }
 }
